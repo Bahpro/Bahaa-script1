2f0f58d6990f373c75ae0273d1a3406f15914e7b
Bahpro <99540481+Bahpro@users.noreply.github.com>
2023-01-10 17:32:43 +0200

    Add Language arabic
    Add Language arabic and eng And many improvements

--- BahaaXD1.cpp
+++ BahaaXD1.cpp
@@ -1,6 +1,50 @@
 #include<iostream>
 
 using namespace std;
+
+int Eng()
+{
+string s;
+    int i = 0;
+    int y = 0;
+    cout<<"enter your text\n";
+    getline (cin, s);
+
+    for (s= s ; s[i] != '\0';)
+    {
+        if(s[i] != ' ')
+        {
+            y++;
+        }
+        i++;
+    }
+    //i = i - y;
+    cout<<"your number is: ";
+    cout<<y<<endl;
+    return 0;
+}
+int Arbc()
+{
+    int i = 0;
+    int y = 0;
+    string s;
+    cout<<"enter your text\n";
+    getline (cin, s);
+    for (s= s ; s[i] != '\0';)
+    {
+        if(s[i] != ' ')
+        {
+            y++;
+        }
+        i++;
+    }
+    //i = i - y;
+    y = y / 2;
+    cout<<"your number is: ";
+    cout<<y<<endl;
+    return 0;
+
+}
 int main()
 {
     for (int y = 0; y <10;y++){
@@ -11,23 +55,26 @@ int main()
     }
 
     cout<<"====================="<<endl;
+    string a;
+    string j;
+
+    cout<<endl<<"Enter E:English for text in english \n \nEnter A:Arabic for text in arabic"<<endl;
+    getline (cin, a);
 
 
-    string s;
-    cout<<"enter your text\n";
-    getline (cin , s);
-    int i = 0;
-    int y = 0;
-    for (s= s ; s[i] != '\0';)
-    {
-        if(s[i] == ' ')
-        {
-            y++;
-        }
-        i++;
-    }
-    i = i - y;
-    cout<<"your number is: ";
-    cout<<i<<endl;
 
+if( a == "e" || a == "E" || a == "English" || a == "english")
+{
+Eng();
+}
+else if( a == "A" || a == "a" || a == "Arabic" || a == "arabic")
+{
+Arbc();
+
+}
+else
+{
+cout<<endl<<"YOU DONT SHOOSE ANY LANGUAGE"<<endl;
+}
+return 0;
 }
